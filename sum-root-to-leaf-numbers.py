03e4eae192da02419f3cc0de27a6374e19855c9b
Andrei Ladyka <andrei@ladyka.by>
2024-04-15 20:48:04 +0300

    129. Sum Root to Leaf Numbers https://leetcode.com/problems/sum-root-to-leaf-numbers/submissions/1233272980?envType=daily-question&envId=2024-04-15

---
+++ sum-root-to-leaf-numbers.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        stack = [root.val]
+        sums = []
+        self.sumValues(root, stack, sums)
+        return sum(sums)
+
+    def sumLeaf(self, root, stack, sums):
+        if root is not None:
+            stack.append(root.val)
+            self.sumValues(root, stack, sums)
+            stack.pop()
+
+    def sumValues(self, root: Optional[TreeNode], stack, sums):
+        if (root.left is None) and (root.right is None):
+            result = int(''.join(map(str, stack)))
+            sums.append(result)
+        self.sumLeaf(root.left, stack, sums)
+        self.sumLeaf(root.right, stack, sums)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.sumNumbers(None))
+    print(s.sumNumbers(TreeNode(val=10)))
+    print(s.sumNumbers(TreeNode(val=10)))
